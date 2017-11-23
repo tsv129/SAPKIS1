bb50e4d6723fe7e913d3f1f0dc47bca76907bdee
tsv129 <32687515+tsv129@users.noreply.github.com>
2017-11-23 22:37:51 +0200

    Lab 7

---
+++ Tkachuk laba SAPKIS 7/Tkachuk laba SAPKIS 1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tkachuk_laba_SAPKIS_1
+{
+    public partial class Form1 : Form
+    {
+        private int[] searchList;
+        private string searched;
+        private int current = 0;
+        private bool act = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ArrInit()
+        {
+            act = true;
+            int index = 0;
+            int i = 0;
+            while (richTextBox1.Text.IndexOf(searched, index) != -1)
+            {
+                index = richTextBox1.Text.IndexOf(searched, index);
+                index += searched.Length;
+                i++;
+            }
+            label1.Text = "Найдено: " + i.ToString();
+            if (i == 0)
+                act = false;
+            else
+            {
+                index = 0;
+                searchList = new int[i];
+                for (int j = 0; j < searchList.Length; j++)
+                {
+                    index = richTextBox1.Text.IndexOf(searched, index);
+                    searchList[j] = index;
+                    index += searched.Length;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            searched = textBox1.Text;
+            ArrInit();
+            Selecter();
+        }
+
+        private void Selecter()
+        {
+            if (act)
+            {
+                richTextBox1.SelectionStart = richTextBox1.Text.IndexOf(searched, searchList[current]);
+                richTextBox1.SelectionLength = searched.Length;
+                richTextBox1.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (act)
+            {
+                if (current != 0)
+                    current--;
+                Selecter();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (act)
+            {
+                if (searchList.Length - 1 != current)
+                    current++;
+                Selecter();
+            }
+        }
+    }
+}

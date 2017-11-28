1d80e448c9c19c55a2a330fff3c058815dcf1753
tsv129 <32687515+tsv129@users.noreply.github.com>
2017-11-28 23:41:59 +0200

    RGZ upload

---
+++ Spichki/Spichki/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Spichki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 154);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Новая игра";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(294, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1 спичка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(375, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "2 спички";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(456, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "3 спички";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(537, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4 спички";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 389);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(600, 168);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(600, 168);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(254, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ваше поле";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(231, 536);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Поле компьютера";
+            this.label2.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 23);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(142, 14);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 35);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Случайное";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 569);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Matches";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Spichki/Spichki/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spichki
+{
+    // TODO:
+    // Кликать на спички
+    public partial class Form1 : Form
+    {
+        Brush BrownBrush = Brushes.Brown;
+        Brush YellowBrush = Brushes.Yellow;
+        int kolvo;
+        int space;
+        int basekol;
+        short[] spichki = new short[30];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawMatch()
+        {
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            Graphics gp = Graphics.FromHwnd(pictureBox2.Handle);
+            Graphics gc = Graphics.FromHwnd(pictureBox3.Handle);
+            g.Clear(SystemColors.Control);
+            gc.Clear(SystemColors.Control);
+            gp.Clear(SystemColors.Control);
+            for (int i = 0; i < basekol; i++)
+                switch (spichki[i])
+                {
+                    case 0:
+                        {
+                            g.FillRectangle(YellowBrush, ((i+1) * space)+1, 20, 10, 110);
+                            g.FillEllipse(BrownBrush, ((i+1) * space), 10, 12, 15);
+                            break;
+                        }
+                    case -1:
+                        {
+                            gc.FillRectangle(YellowBrush, ((i+1) * space)+1, 25, 10, 110);
+                            gc.FillEllipse(BrownBrush, ((i+1) * space), 15, 12, 15);
+                            break;
+                        }
+                    case 1:
+                        {
+                            gp.FillRectangle(YellowBrush, ((i+1) * space)+1, 35, 10, 110);
+                            gp.FillEllipse(BrownBrush, ((i+1) * space), 25, 12, 15);
+                            break;
+                        }
+                }
+        }
+
+        private void ButtonSwap(Boolean b)
+        {
+            button2.Enabled = b;
+            button3.Enabled = b;
+            button4.Enabled = b;
+            button5.Enabled = b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kolvo = basekol = Convert.ToInt32(numericUpDown1.Value);
+            space = 600 / (kolvo+1);
+            for (int y = 0; y < basekol; y++)
+                spichki[y] = 0;
+            DrawMatch();
+            ButtonSwap(true);
+            label1.Visible = true;
+            label2.Visible = true;
+        }
+
+        private bool WinCheck(int p)
+        {
+            if (kolvo <= 0)
+            {
+                if (p == -1)
+                {
+                    MessageBox.Show("Вы проиграли");
+                    ButtonSwap(false);
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Вы выйграли");
+                    ButtonSwap(false);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static int RNG(int k)
+        {
+            Random rng = new Random();
+            return 1+rng.Next(k-1);
+        }
+
+        private void BotTurn()
+        {
+            switch(kolvo % 5)
+            {
+                case 0:
+                    {
+                        int b = RNG(5);
+                        kolvo -= b;
+                        Changer(kolvo, (byte)b, -1);
+                        DrawMatch();
+                        break;
+                    }
+                case 1:
+                    {
+                        kolvo -= 1;
+                        Changer(kolvo, 1, -1);
+                        DrawMatch();
+                        break;
+                    }
+                case 2:
+                    {
+                        kolvo -= 2;
+                        Changer(kolvo, 2, -1);
+                        DrawMatch();
+                        break;
+                    }
+                case 3:
+                    {
+                        kolvo -= 3;
+                        Changer(kolvo, 3, -1);
+                        DrawMatch();
+                        break;
+                    }
+                case 4:
+                    {
+                        kolvo -= 4;
+                        Changer(kolvo, 4, -1);
+                        DrawMatch();
+                        break;
+                    }
+            }
+            WinCheck(-1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clickbtn(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clickbtn(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clickbtn(3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Clickbtn(4);
+        }
+
+        private void Clickbtn(Byte b)
+        {
+            kolvo -= b;
+            Changer(kolvo, b, 1);
+            DrawMatch();
+            if (!WinCheck(1))
+                BotTurn();
+        }
+
+        private void Changer(int kol, Byte take, short player)
+        {
+            for(; kol < kolvo + take; kol++)
+            {
+                spichki[kol] = player;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sdfdsfs");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 10 + RNG(20);
+        }
+    }
+}
